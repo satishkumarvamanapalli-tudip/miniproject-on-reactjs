e5ed1e218223524c5b2adf3b170add421300ecfb
satishkumarvamanapalli-tudip <52527206+satishkumarvamanapalli-tudip@users.noreply.github.com>
2019-08-22 18:45:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import  {Provider} from 'react-redux';
+import {createStore,applyMiddleware,combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import movie from './redux/Reducers'
+import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const rootReducer = combineReducers({
+    movie
+});
+const store = createStore(rootReducer,applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
